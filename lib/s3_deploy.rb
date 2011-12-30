cfb16aa69993775d4fcd39d29861178e91af39a8
Joe Fiorini <joe@joefiorini.com>
2011-12-30 12:47:34 -0500

    User performs very basic S3 deploy

--- lib/s3_deploy.rb
+++ lib/s3_deploy.rb
@@ -1,5 +1,9 @@
 require "s3_deploy/version"
 
 module S3Deploy
-  # Your code goes here...
+  def self.deploy(options={})
+    to = options.delete(:to)
+    from = options.delete(:from)
+    `s3cmd sync --delete-removed  #{from}/ #{to}`
+  end
 end
